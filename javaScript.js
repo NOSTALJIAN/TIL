9cac9b1ced732df7cefbdc4e0b7076dc18751cb1
NOSTALJIAN <nostall.jian@gmail.com>
2022-12-06 16:37:24 +0900

    TIL

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,9 @@
-// 문장
-var x; //  변수 선언
-message = 'H1, ' + name; //  표현식의 대입
-counter++; //  변수 값 증가
-prompt('이름을 입력하시오.'); //  함수 호출
+let x = 'outer x';
+{
+  let x = 'inner x';
+  let y = 'inner y';
+  console.log(x);
+  console.log(y);
+}
+console.log(x);
+console.log(y);
